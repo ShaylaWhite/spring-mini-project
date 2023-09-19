daecf3f27a0b08c3d68b624a624f6d73b11ae922
Shayla <shayladwhite1@gmail.com>
2023-09-19 14:09:31 -0700

    annotated the user class to define as a JPA entity

--- project/src/main/java/com/example/project/model/User.java
+++ project/src/main/java/com/example/project/model/User.java
@@ -1,4 +1,11 @@
 package com.example.project.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+//These annotations mark the class as a JPA entity and specify the database table name.
+
 public class User {
 }
