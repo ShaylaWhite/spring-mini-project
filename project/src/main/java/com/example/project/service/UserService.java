9966de4fe331e5bb9130b90607afbfd80305ff4c
Shayla <shayladwhite1@gmail.com>
2023-09-19 15:05:20 -0700

    Added the UserService Constructor to the UserService class

--- project/src/main/java/com/example/project/service/UserService.java
+++ project/src/main/java/com/example/project/service/UserService.java
@@ -5,6 +5,10 @@ import com.example.project.repository.UserRepository;
 
 public class UserService {
     private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
     //declares a private, final field userRepository of type UserRepository in the UserService class, which will hold the injected UserRepository bean, and once initialized, its value cannot be changed
 
     public User findUserByEmailAddress(String emailAddress) {
