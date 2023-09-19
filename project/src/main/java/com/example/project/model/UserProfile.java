73da66615bf1de0a387d875f47eab10fea9d1e22
Shayla <shayladwhite1@gmail.com>
2023-09-19 14:46:57 -0700

    update the phase 1 steps

--- project/src/main/java/com/example/project/model/UserProfile.java
+++ project/src/main/java/com/example/project/model/UserProfile.java
@@ -7,6 +7,7 @@ import java.util.Date;
 @Table(name = "profiles")
 public class UserProfile {
 
+//annotations are used to define the attributes (fields) of a Java class that represents an entity in a database
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -20,10 +21,12 @@ public class UserProfile {
     @Column(nullable = false)
     private String location;
 
+    //This annotation defines a one-to-one relationship between this entity (UserProfile) and another entity (User). It indicates that each UserProfile is associated with one User.
     @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id")          // the foreign key column in the database table that links the UserProfile entity to the User entity
     private User user;
 
+    // Getters and setters are Java methods used to access and modify the private fields (attributes) of a class. They are part of the JavaBean convention, which is a naming convention for methods used to manipulate the properties of a Java class.
     public String getFullName() {
         return fullName;
     }
