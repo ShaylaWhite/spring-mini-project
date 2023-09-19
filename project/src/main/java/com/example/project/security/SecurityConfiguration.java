35f4fcb20cff2d9e1b1ec57f2080e5585b26bf73
Shayla <shayladwhite1@gmail.com>
2023-09-19 16:46:04 -0700

    added the @cnfiguration to the config secuirty file also implmented auth,brpyt methods

--- project/src/main/java/com/example/project/security/SecurityConfiguration.java
+++ project/src/main/java/com/example/project/security/SecurityConfiguration.java
@@ -1,4 +1,42 @@
 package com.example.project.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/auth/users", "/auth/users/login/", "/auth/users/register/").permitAll()
+                .antMatchers("/h2-console/**").permitAll()
+                .anyRequest().authenticated()
+                .and().sessionManagement()
+                .and().csrf().disable()
+                .headers().frameOptions().disable();
+        return http.build();
+
+    }
+
+    // This method defines the authentication manager configuration.
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        // Configure and return the AuthenticationManager instance
+        // (Details of the configuration are typically provided in the 'authConfig' object)
+        // The AuthenticationManager is responsible for authenticating users.
+        // You may need to implement or configure it based on your authentication requirements.
+        return authConfig.getAuthenticationManager();
+    }
 }
+
