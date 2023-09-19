36cb604cdd9c11e4de848dd1733920a72ee4232e
Shayla <shayladwhite1@gmail.com>
2023-09-19 16:23:46 -0700

    added the applicaton properties

--- project/src/main/java/com/example/project/controller/UserController.java
+++ project/src/main/java/com/example/project/controller/UserController.java
@@ -7,13 +7,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
+//A RestController in Spring is a specialized version of a @Controller that is used to define RESTful web services. It's a common annotation used in Spring MVC applications to indicate that a particular class serves the role of a controller in a RESTful architectur
 @RestController // This annotation specifies that this class is a REST controller.
 @RequestMapping(path = "/auth/users") // This sets the base path for all endpoints in this controller.
 public class UserController {
 
     private UserService userService; // Declare a private field to hold the UserService instance.
 
-    // Constructor to inject the UserService dependency.
+                                        // Constructor to inject the UserService dependency.
     public UserController(UserService userService) {
         this.userService = userService;
     }
@@ -24,4 +27,3 @@ public class UserController {
         return userService.createUser(userObject);
     }
 }
-//A RestController in Spring is a specialized version of a @Controller that is used to define RESTful web services. It's a common annotation used in Spring MVC applications to indicate that a particular class serves the role of a controller in a RESTful architectur
--- project/src/main/java/com/example/project/service/UserService.java
+++ project/src/main/java/com/example/project/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.project.service;
 
+import com.example.project.exception.InformationExistException;
 import com.example.project.model.User;
 import com.example.project.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
