0e494665fcd1d9a98af17342e457e53b3731ac0e
Shayla <shayladwhite1@gmail.com>
2023-09-19 14:30:22 -0700

    Added the Column annotations to the User Profile Class

--- project/src/main/java/com/example/project/model/UserProfile.java
+++ project/src/main/java/com/example/project/model/UserProfile.java
@@ -1,10 +1,35 @@
 package com.example.project.model;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "profiles")
 public class UserProfile {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = false)
+    private Date dateOfBirth;
+
+    @Column(nullable = false)
+    private String location;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
