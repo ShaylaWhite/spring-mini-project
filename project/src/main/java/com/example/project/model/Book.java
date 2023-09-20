596f5192cf8db11414d65e30ee0de03c403d7e99
Shayla <shayladwhite1@gmail.com>
2023-09-19 18:43:29 -0700

    Est the fields for the Book class and its ERD relationships

--- project/src/main/java/com/example/project/model/Book.java
+++ project/src/main/java/com/example/project/model/Book.java
@@ -1,4 +1,33 @@
 package com.example.project.model;
 
+@Entity
+@Table(name = "books")
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id", nullable = false)
+    private Author author;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @Column(nullable = false)
+    private String isbn;
+
+    @Column(nullable = false)
+    private boolean available;
+
+    // Add a relationship mapping to represent the "Book belongs to a User (Borrower)" relationship.
+    @ManyToOne
+    @JoinColumn(name = "borrower_id")
+    private User borrower;
+
+    // Constructors, getters, setters, and other methods.
 }
