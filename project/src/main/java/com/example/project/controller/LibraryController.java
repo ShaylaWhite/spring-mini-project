5543dc22a813b2d01b1b1f07f3506b439efd6b4a
Shayla <shayladwhite1@gmail.com>
2023-09-19 19:34:25 -0700

    Added the params forthe GET mthods of the Controller Class

--- project/src/main/java/com/example/project/controller/LibraryController.java
+++ project/src/main/java/com/example/project/controller/LibraryController.java
@@ -18,24 +18,31 @@ public class LibraryController {
     @Autowired
     private LibraryService libraryService;
 
-    //Books
-// Get all books
+    /**
+     * Get a list of all books.
+     *
+     * @return List<Book> A list of all books.
+     */
     @GetMapping(path = "/books") // GET http://localhost:9092/api/library/books
     public List<Book> getBooks() {
         return libraryService.getBooks();
     }
 
-    //Authors
-// Get all authors
+    /**
+     * Get a list of all authors.
+     *
+     * @return List<Author> A list of all authors.
+     */
     @GetMapping(path = "/authors") // GET http://localhost:9092/api/library/authors
     public List<Author> getAuthors() {
         return libraryService.getAuthors();
     }
 
-    // Categories
-
-    // Get all categories
-
+    /**
+     * Get a list of all categories.
+     *
+     * @return List<Category> A list of all categories.
+     */
     @GetMapping(path = "/categories") // GET http://localhost:9092/api/library/categories
     public List<Category> getCategories() {
         return libraryService.getCategories();
