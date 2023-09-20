af45491804e945d9246bbcd375d19da5afdf756e
Shayla <shayladwhite1@gmail.com>
2023-09-19 18:27:40 -0700

    added the getters and setters for the category class

--- project/src/main/java/com/example/project/model/Category.java
+++ project/src/main/java/com/example/project/model/Category.java
@@ -1,6 +1,8 @@
 package com.example.project.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -8,13 +10,36 @@ import javax.persistence.Table;
 @Table(name = "categories")
 public class Category {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String name;
+
+    // Constructors, getters, and setters
+
+    public Category() {
+        // Default constructor
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    // Getters and setters for id and name
+
+    public Long getId() {
+        return id;
+    }
 
     public void setId(Long id) {
         this.id = id;
     }
 
-    public Long getId() {
-        return id;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
