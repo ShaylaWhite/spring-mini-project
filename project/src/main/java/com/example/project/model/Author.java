0c30a47278234f87a968bcfcf2ed20a88cabe52b
Shayla <shayladwhite1@gmail.com>
2023-09-19 18:40:38 -0700

    Created the getters and Setters for the Author class

--- project/src/main/java/com/example/project/model/Author.java
+++ project/src/main/java/com/example/project/model/Author.java
@@ -16,3 +16,28 @@ public class Author {
     // Add a relationship mapping to represent the "Author can have many Books" relationship.
     @OneToMany(mappedBy = "author")
     private List<Book> books;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+}
