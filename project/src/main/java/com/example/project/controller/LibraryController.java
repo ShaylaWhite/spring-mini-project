8444ce6afd75efee7725b6328598db681bac7e2d
Shayla <shayladwhite1@gmail.com>
2023-09-19 19:05:14 -0700

    Created theLibrary Service autowire for the Controller Class

--- project/src/main/java/com/example/project/controller/LibraryController.java
+++ project/src/main/java/com/example/project/controller/LibraryController.java
@@ -1,9 +1,14 @@
 package com.example.project.controller;
 
+import com.example.project.service.LibraryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/library")
 public class LibraryController {
+
+    @Autowired
+    private LibraryService libraryService;
 }
