a719e2f3d2f0369f26a3bff97f447e243494d5c2
Shayla <shayladwhite1@gmail.com>
2023-09-19 17:05:28 -0700

    generated the getters and setters for the login request class

--- project/src/main/java/com/example/project/request/LoginRequest.java
+++ project/src/main/java/com/example/project/request/LoginRequest.java
@@ -17,4 +17,20 @@ public class LoginRequest {
     private String password;
 
     // Getters and setters for emailAddress and password
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
