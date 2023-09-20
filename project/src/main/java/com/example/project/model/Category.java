73ef47d9f739f1d133ced9ef7e75790946db65e4
Shayla <shayladwhite1@gmail.com>
2023-09-19 18:30:00 -0700

    created the ook model class

--- project/src/main/java/com/example/project/model/Category.java
+++ project/src/main/java/com/example/project/model/Category.java
@@ -1,10 +1,6 @@
 package com.example.project.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "categories")
@@ -14,6 +10,9 @@ public class Category {
     private Long id;
 
     private String name;
+
+//    @ManyToMany(mappedBy = "categories")
+//    private Set<Book> books = new HashSet<>();
 
     // Constructors, getters, and setters
 
