720d7443a6a24998b1e99b968c2d6c0f0f4532d6
Shayla <shayladwhite1@gmail.com>
2023-09-19 17:04:39 -0700

    added the emal address and password fields to the login request class

--- project/src/main/java/com/example/project/request/LoginRequest.java
+++ project/src/main/java/com/example/project/request/LoginRequest.java
@@ -1,4 +1,20 @@
 package com.example.project.request;
 
+/**
+ * LoginRequest is a Data Transfer Object (DTO) used for capturing user login information.
+ * It contains fields for the user's email address and password.
+ */
 public class LoginRequest {
+
+    /**
+     * The user's email address for login.
+     */
+    private String emailAddress;
+
+    /**
+     * The user's password for login.
+     */
+    private String password;
+
+    // Getters and setters for emailAddress and password
 }
