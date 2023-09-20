b747264cebfde5806ad34ed70640ae0b692318c5
Shayla <shayladwhite1@gmail.com>
2023-09-19 19:16:00 -0700

    Establised Each Model role in the Service Class

--- project/src/main/java/com/example/project/service/LibraryService.java
+++ project/src/main/java/com/example/project/service/LibraryService.java
@@ -1,4 +1,35 @@
 package com.example.project.service;
 
+import com.example.project.repository.AuthorRepository;
+import com.example.project.repository.BookRepository;
+import com.example.project.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class LibraryService {
+    private final CategoryRepository categoryRepository;
+    private final BookRepository bookRepository;
+    private final AuthorRepository authorRepository;
+//Constructor injection allows you to provide instances of these repositories when creating a LibraryService object,
+    @Autowired
+    public LibraryService(
+            CategoryRepository categoryRepository,
+            BookRepository bookRepository,
+            AuthorRepository authorRepository) {
+        this.categoryRepository = categoryRepository;
+        this.bookRepository = bookRepository;
+        this.authorRepository = authorRepository;
+    }
+
+    // BOOK OPERATIONS
+
+
+    // AUTHOR OPERATIONS
+
+
+    // CATEGORY OPERATIONS
 }
