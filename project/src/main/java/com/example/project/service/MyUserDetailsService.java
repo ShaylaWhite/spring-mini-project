a63ef4812a0a4b204cc1897b5805c4695405f79b
Shayla <shayladwhite1@gmail.com>
2023-09-19 17:56:36 -0700

    created myUserService Class in the service package

---
+++ project/src/main/java/com/example/project/service/MyUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.example.project.service;
+
+import com.example.project.model.User;
+import com.example.project.service.UserService;
+import com.example.project.security.MyUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String emailAddress) throws UsernameNotFoundException {
+        User user = userService.findUserByEmailAddress(emailAddress);
+        return new MyUserDetails(user);
+    }
+}
