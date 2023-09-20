46c6046bb12707432d83621dd38c82626215d94e
Shayla <shayladwhite1@gmail.com>
2023-09-19 18:38:37 -0700

    Est the table and attributes fields for the author class model

--- project/src/main/java/com/example/project/model/Author.java
+++ project/src/main/java/com/example/project/model/Author.java
@@ -1,4 +1,15 @@
 package com.example.project.model;
 
+@Entity
+@Table(name = "authors")
 public class Author {
-}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    // Add a relationship mapping to represent the "Author can have many Books" relationship.
+    @OneToMany(mappedBy = "author")
+    private List<Book> books;
