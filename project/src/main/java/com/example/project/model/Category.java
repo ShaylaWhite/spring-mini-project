a383b287bcf3e9bd974f186424369d98b3bc0ebb
Shayla <shayladwhite1@gmail.com>
2023-09-19 18:25:58 -0700

    created the bases of the category model class est. the JPA context

--- project/src/main/java/com/example/project/model/Category.java
+++ project/src/main/java/com/example/project/model/Category.java
@@ -1,4 +1,20 @@
 package com.example.project.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categories")
 public class Category {
+    @Id
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
